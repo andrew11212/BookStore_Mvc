9a52cb64cd81f8f55dbf0f58d3b50736065a8b5c
Andrew Atef <Andrew.atef299@gmail.com>
2024-09-07 20:52:42 +0300

    Add Category index View

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+	public class CategoryController : Controller
+	{
+        private readonly ApplicationDBContext _Context;
+        public CategoryController(ApplicationDBContext context)
+        {
+            _Context = context;
+        }
+		public IActionResult Index()
+		{
+			var category = _Context.Categories.ToList();
+			return View(category);
+		}
+	}
+}
