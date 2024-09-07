284cc919051ddcdf78105ea5c710f9739dbe868c
Andrew Atef <Andrew.atef299@gmail.com>
2024-09-07 16:14:32 +0300

    CreateDb

---
+++ BulkyWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+	public class ApplicationDBContext:DbContext
+	{
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) 
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+		public DbSet<Category> Categories { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
